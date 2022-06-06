5e451f1392fdc510b390cbd42a64a7855da5a58e
RitikKumar202 <ritik2002july@gmail.com>
2022-06-06 09:52:06 +0530

    6/6/22

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  console.log(req)
-  res.json({name:"RITIK KUMAR"})
+  res.render('index', { title: 'Fake Institute of Technology' });
+  // console.log(req)
+  // res.json({name:"RITIK KUMAR"})
+});
+
+router.post('/signup', (req, res) => {
+  var em = req.body.emailInput;
+  res.render('index', { email: em , title:"Product Based Company"});
 });
 
 module.exports = router;
--- routes/student.js
+++ routes/student.js
@@ -8,16 +8,11 @@ router.get('/data', function (req, res, next) {
   res.json({ name: "KCC Tier-3 College" })
 });
 
-router.post('/signup', (req, res) => {
-  var em = req.body.emailInput;
-  res.render('student', { email: em });
-});
-
 router.get('/:da-:ta-:ch', function (req, res, next) {
   // res.render('index', { title: 'Express' });
   console.log(req.params.da)
   // res.json({name:req.params.da + " and " + req.params.ta + " are bestfriends"})
-  res.render('student', { name: req.params.da, verb: req.params.ta, name1: req.params.ch })
+  res.render('student', { name: req.params.da, verb: req.params.ta, name1: req.params.ch, title:"INDIA" })
 });
 
 router.get('/:da-:ta', function (req, res, next) {
