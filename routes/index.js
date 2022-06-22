c64e7169db881a42a7677ce5a5f2bf1368f37071
RitikKumar202 <ritik2002july@gmail.com>
2022-06-22 15:55:53 +0530

    calc update

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,35 @@ router.get('/', function (req, res, next) {
   // res.json({name:"RITIK KUMAR"})
 });
 
+router.post('/add', function (req, res, next) {
+  console.log("Adding two number");
+  console.log(req.body);
+  var num1 = Number(req.body.a);
+  var num2 = Number(req.body.b);
+  var result = num1 + num2;
+  console.log(result);
+  // res.json(result);
+  res.render('index', { total: result });
+})
+
+router.post('/sub', function (req, res, next) {
+  console.log(req.body);
+  var num1 = Number(req.body.a);
+  var num2 = Number(req.body.b);
+  var result = num1 - num2;
+  console.log(result);
+  res.render('index', { total: result });
+})
+
+router.post('/mul', function (req, res, next) {
+  console.log(req.body);
+  var num1 = Number(req.body.a);
+  var num2 = Number(req.body.b);
+  var result = num1 * num2;
+  console.log(result);
+  res.render('index', { total: result });
+})
+
 router.post('/signup', (req, res) => {
   var em = req.body.emailInput;
   res.render('index', { email: em, name: req.body.nameInput, title: "Product Based Company" });
