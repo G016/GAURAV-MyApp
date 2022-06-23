31e7960af5de4d535a9569c7696fedb9862450ad
RitikKumar202 <ritik2002july@gmail.com>
2022-06-23 17:20:50 +0530

    stored data in database in tabular form

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var con = mysql.createPool({
+  connectionLimit: 60,
+  host: 'easylearning.guru',
+  user: 'kcc_student',
+  password: 'Kccitm.edu.in1',
+  database: 'kccStudent'
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -13,30 +22,56 @@ router.all('/add', function (req, res, next) {
   console.log(req.body);
   var num1 = parseFloat(req.body.a);
   var num2 = parseFloat(req.body.b);
+  var optr = "+";
   console.log(num1);
   console.log(num2);
   var result = num1 + num2;
   console.log(result);
   res.json(result);
-})
+  con.getConnection(function (err, connection) {
+    connection.query("INSERT INTO ritik_cal (num1, num2 , op, res) VALUES ('" + num1 + "', '" + num2 + "','" + optr + "','" + result + "')", function (err, rows) {
+      connection.release();
+      if (err) throw err;
+      else console.log(rows.length);
+    });
+  });
+});
 
 router.all('/sub', function (req, res, next) {
+  console.log("Subtracting two number");
   console.log(req.body);
   var num1 = parseFloat(req.body.a);
   var num2 = parseFloat(req.body.b);
+  var optr = "-";
   var result = num1 - num2;
   console.log(result);
   res.json(result);
-})
+  con.getConnection(function (err, connection) {
+    connection.query("INSERT INTO ritik_cal (num1, num2 , op, res) VALUES ('" + num1 + "', '" + num2 + "','" + optr + "','" + result + "')", function (err, rows) {
+      connection.release();
+      if (err) throw err;
+      else console.log(rows.length);
+    });
+  });
+});
 
 router.all('/mul', function (req, res, next) {
+  console.log("Multiplying two number");
   console.log(req.body);
   var num1 = parseFloat(req.body.a);
   var num2 = parseFloat(req.body.b);
+  var optr = "*";
   var result = num1 * num2;
   console.log(result);
   res.json(result);
-})
+  con.getConnection(function (err, connection) {
+    connection.query("INSERT INTO ritik_cal (num1, num2 , op, res) VALUES ('" + num1 + "', '" + num2 + "','" + optr + "','" + result + "')", function (err, rows) {
+      connection.release();
+      if (err) throw err;
+      else console.log(rows.length);
+    });
+  });
+});
 
 router.post('/signup', (req, res) => {
   var em = req.body.emailInput;
